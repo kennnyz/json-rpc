0f0e6ba0a1ac4f74e7e4c3fcff51225fd4b4f8e2
muhtarov.m <muhtarov.m@wildberries.work>
2023-06-23 18:46:55 +0300

    readme

--- internal/app/app.go
+++ internal/app/app.go
@@ -6,15 +6,12 @@ import (
 	"github.com/kennnyz/lamoda/internal/server/http_server"
 	"net/rpc/jsonrpc"
 	"os"
-	"time"
-
 	//"github.com/ybbus/jsonrpc/v3"
 	"log"
 )
 
 func Run(configPath string) {
 	tcpAddr := os.Getenv("RCP_ADDRESS")
-	time.Sleep(30 * time.Second)
 	RpcClient, err := jsonrpc.Dial("tcp", tcpAddr)
 	if err != nil {
 		log.Fatal(err)
